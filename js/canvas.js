0dd611c1a39cbfc36f77855293e56d51b897f1ca
imgss <838632526@qq.com>
2017-01-14 20:46:15 +0800

    优化canvas动画

--- js/canvas.js
+++ js/canvas.js
@@ -1,8 +1,10 @@
 (function() { //来自网络
     var canvas = document.querySelector('canvas'),
-        header = document.querySelector('.page-header');
+        header = document.querySelector('.page-header'),
+        dotMaxRad = 5,
+        maxCount = 100;
     ctx = canvas.getContext('2d');
-    ctx.lineWidth = .3;
+    ctx.lineWidth = .2;
     ctx.strokeStyle = (new Color(150)).style;
 
     var mousePosition = {
@@ -20,7 +22,7 @@
     function setWidthHeight() { //dot的数量随窗口变化而变化
         canvas.width = parseFloat(window.getComputedStyle(header, null).width);
         canvas.height = parseFloat(window.getComputedStyle(header, null).height);
-        dots.nb = Math.min(300, Math.max(50, parseInt(200 * canvas.width / 1000)));
+        dots.nb = Math.min(maxCount, Math.max(50, parseInt(200 * canvas.width / 1000)));
         // 重点在于改变要显示的dots数量之后，如果
         // createDots没有创建那么多，就会有空引用，所以，初始化时就创建300个dots,在
         // 绘制时根据resize取出50——300个进行绘制，这样不会出现dot.y没有定义的错误
@@ -65,12 +67,13 @@
         this.x = Math.random() * canvas.width;
         this.y = Math.random() * canvas.height;
 
-        this.vx = -.5 + Math.random(); //dot的x速度
-        this.vy = -.5 + Math.random(); //dot的y速度
+        this.vx = -.3 + 0.6 * Math.random(); //dot的x速度
+        this.vy = -.3 + 0.6 * Math.random(); //dot的y速度
 
-        this.radius = Math.random() * 2;
+        this.radius = Math.random() * dotMaxRad;
 
         this.color = new Color();
+        this.connectNum = 0; //记录有几个点与之相连
     }
 
     Dot.prototype = { //每一个实例继承的方法
@@ -84,7 +87,7 @@
 
     function createDots() { //dots数组
         setWidthHeight();
-        for (i = 0; i < 300; i++) {
+        for (i = 0; i < maxCount; i++) {
             dots.array.push(new Dot());
         }
     }
@@ -126,6 +129,34 @@
         }
     }
 
+    function connectDot() { //连结dots,自己写的,每个点只能连4个别的点
+        for (i = 0; i < dots.nb; i++) {
+            for (j = 0; j < dots.nb; j++) {
+                i_dot = dots.array[i];
+                j_dot = dots.array[j];
+                if (i_dot.connectNum < 3 && j_dot.connectNum < 3 && distance(i_dot, j_dot) < 60) {
+                    ctx.beginPath();
+                    ctx.strokeStyle = averageColorStyles(i_dot, j_dot);
+                    ctx.lineWidth = .2;
+                    ctx.moveTo(i_dot.x, i_dot.y);
+                    ctx.lineTo(j_dot.x, j_dot.y);
+                    ctx.stroke();
+                    ctx.closePath();
+                    i_dot.connectNum++;
+                }
+            }
+        }
+        for (i = 0; i < dots.nb; i++) {
+            dots.array[i].connectNum = 0;
+        }
+
+    }
+
+    function distance(d1, d2) {
+        return Math.sqrt((d2.x - d1.x) * (d2.x - d1.x) + (d2.y - d1.y) * (d2.y - d1.y));
+
+    }
+
     function drawDots() {
         for (i = 0; i < dots.nb; i++) {
             var dot = dots.array[i];
@@ -139,7 +170,7 @@
         ctx.rect(0, 0, canvas.width, canvas.height); //画点
         ctx.fill();
         moveDots();
-        connectDots();
+        connectDot();
         drawDots();
 
         requestAnimationFrame(animateDots);
--- js/index.js
+++ js/index.js
@@ -1,12 +1,12 @@
-var ul = document.querySelector('.main-content ul');
-ul.addEventListener('mouseover', function(e) {
+var div = document.querySelector('section div');
+div.addEventListener('mouseover', function(e) {
     e.stopPropagation();
 
     if (e.target.nodeName == 'A') {
         e.target.nextSibling.className = 'animated fadeIn';
     };
 });
-ul.addEventListener('mouseout', function(e) {
+div.addEventListener('mouseout', function(e) {
     e.stopPropagation();
 
     if (e.target.nodeName == 'A') {
