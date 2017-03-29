c2a894d06acf1dbb7694e616af6561ab73c1f066
imgss <838632526@qq.com>
2017-03-29 22:43:22 +0800

    modify page

--- js/canvas.js
+++ js/canvas.js
@@ -48,7 +48,7 @@
     }
 
     function mixComponents(comp1, weight1, comp2, weight2) {
-        return (comp1 * weight1 + comp2 * weight2) / (weight1 + weight2);
+        return(comp1 * weight1 + comp2 * weight2) / (weight1 + weight2);
     }
 
     function averageColorStyles(dot1, dot2) { //生成两点的中间色值
@@ -93,20 +93,20 @@
 
     function createDots() { //dots数组
         setWidthHeight();
-        for (i = 0; i < maxCount; i++) {
+        for(i = 0; i < maxCount; i++) {
             dots.array.push(new Dot());
         }
     }
 
     function moveDots() {
-        for (i = 0; i < dots.nb; i++) {
+        for(i = 0; i < dots.nb; i++) {
 
             var dot = dots.array[i];
 
-            if (dot.y < 0 || dot.y > canvas.height) {
+            if(dot.y < 0 || dot.y > canvas.height) {
                 dot.vx = dot.vx;
                 dot.vy = -dot.vy;
-            } else if (dot.x < 0 || dot.x > canvas.width) {
+            } else if(dot.x < 0 || dot.x > canvas.width) {
                 dot.vx = -dot.vx;
                 dot.vy = dot.vy;
             }
@@ -116,13 +116,13 @@
     }
 
     function connectDots() { //连结dots，一个交互作用
-        for (i = 0; i < dots.nb; i++) {
-            for (j = 0; j < dots.nb; j++) {
+        for(i = 0; i < dots.nb; i++) {
+            for(j = 0; j < dots.nb; j++) {
                 i_dot = dots.array[i];
                 j_dot = dots.array[j];
 
-                if ((i_dot.x - j_dot.x) < dots.distance && (i_dot.y - j_dot.y) < dots.distance && (i_dot.x - j_dot.x) > -dots.distance && (i_dot.y - j_dot.y) > -dots.distance) {
-                    if ((i_dot.x - mousePosition.x) < dots.d_radius && (i_dot.y - mousePosition.y) < dots.d_radius && (i_dot.x - mousePosition.x) > -dots.d_radius && (i_dot.y - mousePosition.y) > -dots.d_radius) {
+                if((i_dot.x - j_dot.x) < dots.distance && (i_dot.y - j_dot.y) < dots.distance && (i_dot.x - j_dot.x) > -dots.distance && (i_dot.y - j_dot.y) > -dots.distance) {
+                    if((i_dot.x - mousePosition.x) < dots.d_radius && (i_dot.y - mousePosition.y) < dots.d_radius && (i_dot.x - mousePosition.x) > -dots.d_radius && (i_dot.y - mousePosition.y) > -dots.d_radius) {
                         ctx.beginPath();
                         ctx.strokeStyle = averageColorStyles(i_dot, j_dot);
                         ctx.moveTo(i_dot.x, i_dot.y);
@@ -136,11 +136,11 @@
     }
 
     function connectDot() { //连结dots,自己写的,每个点只能连4个别的点
-        for (i = 0; i < dots.nb; i++) {
-            for (j = 0; j < dots.nb; j++) {
+        for(i = 0; i < dots.nb; i++) {
+            for(j = 0; j < dots.nb; j++) {
                 i_dot = dots.array[i];
                 j_dot = dots.array[j];
-                if (i_dot.connectNum < 3 && j_dot.connectNum < 3 && distance(i_dot, j_dot) < dots.distance) {
+                if(i_dot.connectNum < 3 && j_dot.connectNum < 3 && distance(i_dot, j_dot) < dots.distance) {
                     ctx.beginPath();
                     ctx.strokeStyle = averageColorStyles(i_dot, j_dot);
                     ctx.lineWidth = .2 * tScale;
@@ -152,7 +152,7 @@
                 }
             }
         }
-        for (i = 0; i < dots.nb; i++) {
+        for(i = 0; i < dots.nb; i++) {
             dots.array[i].connectNum = 0;
         }
 
@@ -164,7 +164,7 @@
     }
 
     function drawDots() {
-        for (i = 0; i < dots.nb; i++) {
+        for(i = 0; i < dots.nb; i++) {
             var dot = dots.array[i];
             dot.draw();
         }
