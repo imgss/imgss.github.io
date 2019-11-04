28e11a4e80e2a42db96228843ed6a80e6162a7a1
imgss <838632526@qq.com>
2019-11-04 23:57:39 +0800

    ts

--- demo/p5/camera.js
+++ demo/p5/camera.js
@@ -1,9 +1,16 @@
 let capture;
 windowWidth = window.innerWidth
 function setup() {
-  c = createCanvas(windowWidth, windowWidth * 0.5);
-  capture = createCapture(VIDEO);
-  capture.size(windowWidth, windowWidth * 0.5);
+  c = createCanvas(windowWidth, windowWidth * 0.75);
+  
+  capture = createCapture({
+    audio: false,
+    video: {
+      width: 800,
+      height: 600
+    }
+  });
+  capture.size(windowWidth, windowWidth * 0.75);
   img = loadImage('./hat.png');
   capture.hide();
   document.getElementById('btn').addEventListener('click', function() {
@@ -13,10 +20,11 @@ function setup() {
 
 function draw() {
   background(255);
-  image(capture, 0, 0, windowWidth * 0.75, windowWidth * 0.5);
+  c.drawingContext.scale(-1, 1);
+  image(capture, 0, 0, -windowWidth, windowWidth * 0.75);
 
   // filter(INVERT);
-  image(img, (mouseX > 320 ? 320 : mouseX) - img.width / 2, (mouseY > 240 ? 240 : mouseY) - img.height / 2, img.width / 2, img.height / 2)
+  image(img, -(mouseX > 320 ? 320 : mouseX) - img.width / 2, (mouseY > 240 ? 240 : mouseY) - img.height / 2, img.width / 2, img.height / 2)
 }
 
 function keyPressed() {
